862606dcedde0d29d2af51328a7b825103be9473
David Holmes <phoenix1988@gmail.com>
2019-12-27 21:12:58 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -24,7 +24,7 @@ def print_first_directors_movie_titles
   spielberg = []
     array2 = 0
     while array2 < directors_database[0][:movies].length do
-       spielberg << directors_database[0][:movies][0][:title]
+       spielberg << directors_database[0][:movies][array2][:title]
      array2 =+ 1
      end
   pp spielberg
